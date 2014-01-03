90272e5e9236e93cd6688b70602fd233308992ea
Don Smith <don.c.smith@gmail.com>
2014-01-02 23:31:28 -0600

    tidying up

---
+++ members/don/project-euler/problem-0003.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+#
+# http://en.wikipedia.org/wiki/Prime_factorization
+
+magic = 600851475143
