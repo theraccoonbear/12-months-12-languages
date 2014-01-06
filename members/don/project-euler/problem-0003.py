a103fcba8328962ba1e39ee2b9843e3b6e3f5d3c
Don Smith <don.c.smith@gmail.com>
2014-01-05 18:35:23 -0600

    euler #3

--- members/don/project-euler/problem-0003.py
+++ members/don/project-euler/problem-0003.py
@@ -6,4 +6,33 @@
 #
 # http://en.wikipedia.org/wiki/Prime_factorization
 
+import math
+
 magic = 600851475143
+
+primeStatus = {}
+
+def isPrime(n):
+	if not n in primeStatus: 
+		if n % 2 == 0:
+			primeStatus[n] = False
+		else:
+			upper = int(math.ceil(math.sqrt(n)))
+			primeStatus[n] = True
+			for i in range(upper, 1, - 1):
+				if n % i == 0:
+					primeStatus[n] = False
+					break
+	
+	return primeStatus[n]
+
+
+biggest = 0
+
+for x in range(0, 10000):
+	if isPrime(x):
+		if magic % x == 0:
+			biggest = x
+			
+print "Biggest: " + `biggest`
+
