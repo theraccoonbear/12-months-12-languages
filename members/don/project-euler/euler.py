38dbe123ed24fadcc133a7f5d56795fa53924a46
Don Smith <don.c.smith@gmail.com>
2014-01-06 09:29:39 -0600

    euler #4 + module.  ignorning compiled (.pyc) files

---
+++ members/don/project-euler/euler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import math
+
+primeStatus = {}
+
+def isPrime(n):
+	if not n in primeStatus: 
+		if n % 2 == 0:
+			primeStatus[n] = False
+		else:
+			upper = int(math.ceil(math.sqrt(n)))
+			primeStatus[n] = True
+			for i in range(upper, 1, - 1):
+				if n % i == 0:
+					primeStatus[n] = False
+					break
+	return primeStatus[n]
+
+def isPalindrome(n):
+	n = str(n);
+	length = len(n);
+	half = int(math.floor(length / 2))
+	left = n[0:half];
+	right = (n[length-half:length])[::-1]
+	return left == right
